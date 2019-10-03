6e4a8363b5f563245cfabc2117db3263fdc2b0e8
rochdev <roch.devost@datadoghq.com>
2019-10-03 22:33:08 +0200

    add browserstack

---
+++ karma.bs.conf.js
@@ -0,0 +1,22 @@
+const base = require('./karma.conf')
+
+module.exports = config => {
+  base(config)
+
+  config.set({
+    reporters: ['dots', 'BrowserStack'],
+
+    browsers: ['bs_chrome_mac'],
+
+    // https://github.com/karma-runner/karma-browserstack-launcher
+    customLaunchers: {
+      bs_chrome_mac: {
+        base: 'BrowserStack',
+        browser: 'Chrome',
+        browser_version: '76.0',
+        os: 'Windows',
+        os_version: '10'
+      }
+    }
+  })
+}
